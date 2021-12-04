b56cf495815e46c75a48778c965acc3be57697dd
Benjamin Hodgson <bhodgson@stackoverflow.com>
2021-12-04 09:38:56 -0500

    set testEnvironment: node
    I was getting insonsistent test breakages on my machine.
    Not sure why, perhaps a pkg version mismatch. This seems to fix it

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ const useWebpack5 = process.env.WEBPACK === '5';
 
 module.exports = {
 	preset: 'es-jest',
+	testEnvironment: 'node',
 	moduleNameMapper: useWebpack5
 		? {
 			'^webpack$': 'webpack5',
