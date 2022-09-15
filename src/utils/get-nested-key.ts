c557cf1d3cd9fecb9d6c249910d3e58b9697912c
Florian Bischof <design.falke@gmail.com>
2022-09-15 14:03:31 +0200

    Add support for partial nested keys

--- src/utils/get-nested-key.ts
+++ src/utils/get-nested-key.ts
@@ -1,13 +1,42 @@
 import { LocaleStrings } from '../types';
 
+/**
+ * { error: { notFound: 'Message not found!' } }
+ * 		= __('error.notFound')
+ *
+ * { error: { 'not.found': 'Message not found!' } }
+ * 		= __('error.not.found')
+ *
+ * { error: { 'string.validate': { 'not.found': 'Message not found!' } } }
+ * 		= __('error.string.validate.not.found')
+ */
+
 export function getNestedKey<LocalizedData>(
 	key: string,
 	localeData: LocaleStrings<LocalizedData>,
 ) {
 	const jsonPath = key.split('.');
+	let currentPath = [];
 	let jsonObject = localeData;
 	for (const path of jsonPath) {
-		jsonObject = jsonObject && jsonObject[path] as LocaleStrings<LocalizedData>;
+		if (jsonObject && jsonObject[path]) {
+			jsonObject = jsonObject[path] as LocaleStrings<LocalizedData>;
+			currentPath = [];
+		} else {
+			/*
+			 * Checks if the joined path equals a key. f.ex. `not.found`
+			 */
+			currentPath.push(path);
+			const currentPathString = currentPath.join('.');
+			if (jsonObject && jsonObject[currentPathString]) {
+				jsonObject = jsonObject[currentPathString] as LocaleStrings<LocalizedData>;
+				currentPath = [];
+			}
+		}
+	}
+
+	if (typeof jsonObject !== 'string') {
+		return undefined as LocalizedData;
 	}
 	return jsonObject as LocalizedData;
 }
